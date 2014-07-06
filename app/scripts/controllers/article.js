7d1af211a7ed8aa4b1c82a224193dd1463fbe068
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2014-07-06 08:04:52 +0200

    adds facebook button

--- app/scripts/controllers/article.js
+++ app/scripts/controllers/article.js
@@ -1,13 +1,22 @@
 angular.module('theChurningTimesApp').controller('ArticleCtrl', function ($rootScope, $scope, $routeParams, $timeout, ArticleService) {
-  $timeout(function () { twttr.widgets.load(); });
+  $timeout(function () { twttr.widgets.load(); }); // renders the timeline
 
   ArticleService.fetch($routeParams.id).success(function(data){
     $scope.article = data;
     $rootScope.title = $scope.article.headline;
   });
 
+  var extractURL = function(){ return location.href.replace('#','%23'); };
+
   $scope.shareOnTwitter = function(){
-    open('https://twitter.com/share?' + 'text=' + $scope.article.headline + '&via=churningtimes&url=' + location.href.replace('#','%23'));
+    open('https://twitter.com/share?' + 'text=' + $scope.article.headline + '&via=churningtimes&url=' + extractURL());
+  };
+
+  $scope.shareOnFacebook = function(){
+    open('https://www.facebook.com/dialog/share?app_id=741440062566027&display=popup&href=' +
+      extractURL() +
+      '&redirect_uri=' +
+      extractURL());
   };
 });
 
